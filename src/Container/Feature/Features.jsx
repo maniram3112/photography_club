dd9dd3fa46d351936178eedbdc7212a29137232b
unknown <maniramthota12@gmail.com>
2024-06-21 15:57:25 +0530

    added responsiveness

--- src/Container/Feature/Features.jsx
+++ src/Container/Feature/Features.jsx
@@ -49,102 +49,102 @@ const Features = [
         instaHandle: 'https://example.com/name1',
         instaUserId: '@name_1'
     },
-    {
-        img : images.feature1,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature2,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature3,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature1,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature2,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature3,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature1,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature2,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature3,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature1,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature2,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    },
-    {
-        img : images.feature3,
-        alt: 'img1',
-        name: 'name 1',
-        instaLink: 'https://example.com/img1',
-        instaHandle: 'https://example.com/name1',
-        instaUserId: '@name_1'
-    }
+    // {
+    //     img : images.feature1,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature2,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature3,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature1,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature2,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature3,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature1,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature2,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature3,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature1,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature2,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // },
+    // {
+    //     img : images.feature3,
+    //     alt: 'img1',
+    //     name: 'name 1',
+    //     instaLink: 'https://example.com/img1',
+    //     instaHandle: 'https://example.com/name1',
+    //     instaUserId: '@name_1'
+    // }
 ]
 
 export default Features;
